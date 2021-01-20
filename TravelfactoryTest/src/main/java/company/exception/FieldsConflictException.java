0cf4db4e636e39cc839dee97a62c8625dff852bd
GrigKG <38141067+GrigKG@users.noreply.github.com>
2021-01-20 11:49:11 +0200

    add

---
+++ TravelfactoryTest/src/main/java/company/exception/FieldsConflictException.java
@@ -0,0 +1,18 @@
+package company.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import lombok.NoArgsConstructor;
+
+@ResponseStatus(code = HttpStatus.CONFLICT)
+public class FieldsConflictException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8865719398771533703L;
+	
+	public FieldsConflictException() {
+		super("Mandatory fields are not filled");
+	}
+}
